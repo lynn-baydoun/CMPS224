0f62d5124d9109d66caeac46c89adeeebb34410c
Lynn Baydoun <lkb07@mail.aub.edu>
2025-04-24 23:26:50 +0300

    milestone 3

--- kernel1.cu
+++ kernel1.cu
@@ -1,16 +1,71 @@
-
+#include <cuda.h>
+#include <stdio.h>
 #include "common.h"
 
+#define MAX_COLS 1024  //assume an upper bound for columns
+
+__global__ void kernel1(CSRMatrix d_csrA, CSCMatrix d_cscB, unsigned int* d_rows, unsigned int* d_cols, float* d_vals, unsigned int* d_nnzCounter, int numColsB) {
+    int row = blockIdx.x * blockDim.x + threadIdx.x;
+    if (row >= d_csrA.numRows) return;
+
+    //value accumulation per column
+    float acc[MAX_COLS] = {0};
+
+    for (int idxA = d_csrA.rowPtrs[row]; idxA < d_csrA.rowPtrs[row + 1]; idxA++) {
+        int a_col = d_csrA.colIndices[idxA];
+        float a_val = d_csrA.values[idxA];
+        for (int idxB = d_cscB.colPtrs[a_col]; idxB < d_cscB.colPtrs[a_col + 1]; idxB++) {
+            int b_col = d_cscB.rowIndices[idxB];
+            float b_val = d_cscB.values[idxB];
+            acc[b_col] += a_val * b_val;
+        }
+    }
+
+    //non-zero entries to global memory
+    for (int col = 0; col < numColsB; col++) {
+        if (acc[col] != 0.0f) {
+            unsigned int index = atomicAdd(d_nnzCounter, 1);
+            d_rows[index] = row;
+            d_cols[index] = col;
+            d_vals[index] = acc[col];
+        }
+    }
+}
+
 void spmspm_gpu1(COOMatrix* cooMatrix1, CSRMatrix* csrMatrix1, CSCMatrix* cscMatrix1, COOMatrix* cooMatrix2, CSRMatrix* csrMatrix2, CSCMatrix* cscMatrix2, COOMatrix* cooMatrix3, unsigned int numRows1, unsigned int numRows2, unsigned int numCols2, unsigned int numNonzeros1, unsigned int numNonzeros2) {
+    unsigned int maxNonzeros = numNonzeros1 * numNonzeros2;
+    unsigned int* d_rows;
+    unsigned int* d_cols;
+    float* d_vals;
+    unsigned int* d_nnzCounter;
 
+    cudaMalloc(&d_rows, maxNonzeros * sizeof(unsigned int));
+    cudaMalloc(&d_cols, maxNonzeros * sizeof(unsigned int));
+    cudaMalloc(&d_vals, maxNonzeros * sizeof(float));
+    cudaMalloc(&d_nnzCounter, sizeof(unsigned int));
+    cudaMemset(d_nnzCounter, 0, sizeof(unsigned int));
 
+    int blockSize = 256;
+    int numBlocks = (numRows1 + blockSize - 1) / blockSize;
+    kernel1<<<numBlocks, blockSize>>>(*csrMatrix1, *cscMatrix2, d_rows, d_cols, d_vals, d_nnzCounter, numCols2);
+    cudaDeviceSynchronize();
 
+    //back to COO format
+    unsigned int h_nnz;
+    cudaMemcpy(&h_nnz, d_nnzCounter, sizeof(unsigned int), cudaMemcpyDeviceToHost);
 
+    cooMatrix3->numNonzeros = h_nnz;
+    cudaMallocHost(&cooMatrix3->rows, h_nnz * sizeof(unsigned int));
+    cudaMallocHost(&cooMatrix3->cols, h_nnz * sizeof(unsigned int));
+    cudaMallocHost(&cooMatrix3->values, h_nnz * sizeof(float));
 
+    cudaMemcpy(cooMatrix3->rows, d_rows, h_nnz * sizeof(unsigned int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(cooMatrix3->cols, d_cols, h_nnz * sizeof(unsigned int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(cooMatrix3->values, d_vals, h_nnz * sizeof(float), cudaMemcpyDeviceToHost);
 
-
-
-
-
+    cudaFree(d_rows);
+    cudaFree(d_cols);
+    cudaFree(d_vals);
+    cudaFree(d_nnzCounter);
 }
 
